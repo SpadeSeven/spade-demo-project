21783f042b9318c2164cddf228a6434a8cfe1857
SpadeSeven <939082455@qq.com>
2019-01-05 22:06:46 +0800

    1、新建think4j项目 2、新增.gitingnore

---
+++ java-project/think4j/src/main/java/com/zhang/demo/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2019-01-05.
+ */
+package com.zhang.demo;
---
+++ java-project/think4j/src/main/java/com/zhang/demo/part5/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2019-01-05.
+ *
+ * 初始化与清理
+ */
+package com.zhang.demo.part5;
