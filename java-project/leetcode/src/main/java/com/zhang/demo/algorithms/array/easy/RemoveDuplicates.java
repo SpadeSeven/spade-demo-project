975bbd4cc064be801c278b4c753b96f991ef98d5
SpadeSeven <939082455@qq.com>
2019-03-06 22:45:10 +0800

    26. Remove Duplicates from Sorted Array

---
+++ java-project/leetcode/src/main/java/com/zhang/demo/algorithms/array/easy/RemoveDuplicates.java
@@ -0,0 +1,31 @@
+package com.zhang.demo.algorithms.array.easy;
+
+/**
+ * Created by Administrator on 2019-03-06.
+ */
+
+/**
+ * Given a sorted array nums, remove the duplicates in-place such that each element appear only once
+ * and return the new length.
+ *
+ * Do not allocate extra space for another array, you must do this by modifying the input array
+ * in-place with O(1) extra memory.
+ */
+public class RemoveDuplicates {
+
+  public int removeDuplicates(int[] nums) {
+    if (nums.length == 0) {
+      return 0;
+    }
+    int i = 0;
+    int j = 1;
+    while (j <= nums.length - 1) {
+      if (nums[i] != nums[j]) {
+        nums[i + 1] = nums[j];
+        i++;
+      }
+      j++;
+    }
+    return i + 1;
+  }
+}
---
+++ java-project/leetcode/src/test/java/com/zhang/demo/algorithms/array/easy/RemoveDuplicatesTest.java
@@ -0,0 +1,25 @@
+package com.zhang.demo.algorithms.array.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2019-03-06.
+ */
+public class RemoveDuplicatesTest {
+
+  @Test
+  public void test_1() {
+    RemoveDuplicates remove = new RemoveDuplicates();
+    int[] nums = {1, 1, 2};
+    Assert.assertEquals(2, remove.removeDuplicates(nums));
+  }
+
+  @Test
+  public void test_2() {
+    RemoveDuplicates remove = new RemoveDuplicates();
+    int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    Assert.assertEquals(5, remove.removeDuplicates(nums));
+  }
+
+}
