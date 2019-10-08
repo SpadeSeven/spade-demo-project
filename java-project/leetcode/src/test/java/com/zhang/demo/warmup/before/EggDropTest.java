a79f1e2ae3553b843df8175c8b3c4aecfa824421
SpadeSeven <939082455@qq.com>
2019-10-09 00:14:04 +0800

    热身 鸡蛋掉落

---
+++ java-project/leetcode/src/test/java/com/zhang/demo/warmup/before/EggDropTest.java
@@ -0,0 +1,22 @@
+package com.zhang.demo.warmup.before;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EggDropTest {
+
+  private EggDrop drop;
+
+  @Before
+  public void before() {
+    drop = new EggDrop();
+  }
+
+  @Test
+  public void test() {
+    Assertions.assertThat(drop.superEggDrop(1, 2)).isEqualTo(2);
+    Assertions.assertThat(drop.superEggDrop(2, 6)).isEqualTo(3);
+    Assertions.assertThat(drop.superEggDrop(3, 14)).isEqualTo(4);
+  }
+}
