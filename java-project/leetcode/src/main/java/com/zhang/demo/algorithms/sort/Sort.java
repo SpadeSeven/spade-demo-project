eb8286dc642c57c676fc9418e5b80b280f12de5f
SpadeSeven <939082455@qq.com>
2020-03-29 02:10:50 +0800

    冒泡排序

---
+++ java-project/leetcode/src/main/java/com/zhang/demo/algorithms/sort/Sort.java
@@ -0,0 +1,6 @@
+package com.zhang.demo.algorithms.sort;
+
+public interface Sort {
+
+  int[] sort(int[] arr);
+}
---
+++ java-project/leetcode/src/test/java/com/zhang/demo/algorithms/sort/BubbleSortTest.java
@@ -0,0 +1,17 @@
+package com.zhang.demo.algorithms.sort;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BubbleSortTest {
+
+  private BubbleSort sorter;
+
+  @Before
+  public void setUp() {
+    sorter = new BubbleSort();
+  }
+
+  @Test
+  public void test_Sort() {}
+}
