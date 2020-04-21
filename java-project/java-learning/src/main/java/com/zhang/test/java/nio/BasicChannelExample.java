f8e0710007eb655edbcbdeae03a53def4edb1cdb
SpadeSeven <939082455@qq.com>
2020-04-21 23:27:36 +0800

    加点注释

--- java-project/java-learning/src/main/java/com/zhang/test/java/nio/BasicChannelExample.java
+++ java-project/java-learning/src/main/java/com/zhang/test/java/nio/BasicChannelExample.java
@@ -1,5 +1,33 @@
 package com.zhang.test.java.nio;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class BasicChannelExample {
 
+  public static void main(String[] args) throws IOException {
+    RandomAccessFile file = new RandomAccessFile("prop.txt", "rw");
+    FileChannel channel = file.getChannel();
+
+    // create buffer with capacity of 48 bytes
+    ByteBuffer byteBuffer = ByteBuffer.allocate(48);
+
+    // read into buffer
+    int byteRead = 0;
+    while ((byteRead = channel.read(byteBuffer)) != -1) {
+      // make buffer ready for reading
+      byteBuffer.flip();
+
+      while (byteBuffer.hasRemaining()) {
+        // read 1 byte at a time
+        System.out.println((char) byteBuffer.get());
+      }
+
+      // clean buffer, make buffer ready for writing
+      byteBuffer.clear();
+    }
+    file.close();
+  }
 }
