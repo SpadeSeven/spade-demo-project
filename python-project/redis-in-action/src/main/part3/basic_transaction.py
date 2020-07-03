cbfbdbd8f46ee92bb957af13dd18605844ce0adf
SpadeSeven <939082455@qq.com>
2020-07-03 17:23:21 +0800

    redis-in-acion

---
+++ python-project/redis-in-action/src/main/part3/basic_transaction.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import redis
+import time
+import threading
+
+conn = redis.Redis(host='172.168.10.25', port=6378)
+
+
+def nortans():
+    print(conn.incr("nortans:"))
+    time.sleep(1)
+    conn.incr("nortans:", -1)
+
+
+if __name__ == '__main__':
+    if 1:
+        for i in range(0, 3):
+            threading.Thread(target=nortans()).start()
+        time.sleep(.5)
+
+conn.close()
