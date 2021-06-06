5292a334fe73a1287aac1fa69adc6893b6bed725
SpadeSeven <939082455@qq.com>
2021-06-06 15:34:01 +0800

    spring-cloud 1、创建eureka server项目

---
+++ java-project/spring-cloud/spring-cloud-eureka-server-demo/src/test/java/com/spade/zhang/springcloudeurekaserverdemo/SpringCloudEurekaServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spade.zhang.springcloudeurekaserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaServerDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
