7e64356f001dde871a6e89432df435afbdc7e16e
SpadeSeven <939082455@qq.com>
2021-06-07 00:41:42 +0800

    1、配置中心客戶端接入eureka

--- java-project/spring-cloud/config-client/src/main/java/com/spade/zhang/ConfigClientApplication.java
+++ java-project/spring-cloud/config-client/src/main/java/com/spade/zhang/ConfigClientApplication.java
@@ -2,8 +2,12 @@ package com.spade.zhang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class ConfigClientApplication {
 
   public static void main(String[] args) {
