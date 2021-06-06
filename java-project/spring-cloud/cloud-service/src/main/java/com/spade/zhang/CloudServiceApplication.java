9b1be2a342a85fd5408779cc7c41985777f01c89
SpadeSeven <939082455@qq.com>
2021-06-06 21:25:50 +0800

    1、解决注册eurake为内网IP的问题

--- java-project/spring-cloud/cloud-service/src/main/java/com/spade/zhang/CloudServiceApplication.java
+++ java-project/spring-cloud/cloud-service/src/main/java/com/spade/zhang/CloudServiceApplication.java
@@ -2,10 +2,12 @@ package com.spade.zhang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class CloudServiceApplication {
 
   public static void main(String[] args) {
