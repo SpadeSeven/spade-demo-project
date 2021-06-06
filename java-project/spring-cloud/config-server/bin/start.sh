8604d9fd00f11c53690b4019e2cdec62599ba850
SpadeSeven <939082455@qq.com>
2021-06-06 22:56:42 +0800

    1、 service-zuul 服务

---
+++ java-project/spring-cloud/config-server/bin/start.sh
@@ -0,0 +1,1 @@
+nohup java -Xms128M -Xmx128M -jar /opt/spring-cloud/config-server/lib/config-server-0.0.1-SNAPSHOT.jar &
---
+++ java-project/spring-cloud/service-zuul/bin/start.sh
@@ -0,0 +1,1 @@
+nohup java -Xms128M -Xmx128M -jar /opt/spring-cloud/zuul-service/lib/service-zuul-0.0.1-SNAPSHOT.jar &
