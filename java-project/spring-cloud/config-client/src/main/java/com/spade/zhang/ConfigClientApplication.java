6057696d96e2113915ec09b9be7c65337b42d6f3
SpadeSeven <939082455@qq.com>
2021-06-07 01:01:05 +0800

    1、接入消息總綫

--- java-project/spring-cloud/config-client/src/main/java/com/spade/zhang/ConfigClientApplication.java
+++ java-project/spring-cloud/config-client/src/main/java/com/spade/zhang/ConfigClientApplication.java
@@ -3,11 +3,13 @@ package com.spade.zhang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
+@RefreshScope
 public class ConfigClientApplication {
 
   public static void main(String[] args) {
