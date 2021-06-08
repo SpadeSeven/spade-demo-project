8fdd402b9efb9fb9ee7b100da783e8aa52e6748f
SpadeSeven <939082455@qq.com>
2021-06-08 21:21:47 +0800

    1、解决配置不生效问题

--- java-project/spring-cloud/config-client/src/main/java/com/spade/zhang/controller/HiController.java
+++ java-project/spring-cloud/config-client/src/main/java/com/spade/zhang/controller/HiController.java
@@ -1,10 +1,12 @@
 package com.spade.zhang.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HiController {
 
   @Value("${foo}")
