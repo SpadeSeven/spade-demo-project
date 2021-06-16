f2578f988237927131241af7438a78fd8e23b105
SpadeSeven <939082455@qq.com>
2021-06-16 22:00:24 +0800

    实现 HystrixDashboard

--- java-project/spring-cloud/service-client/src/main/java/com/spade/zhang/ServiceClientApplication.java
+++ java-project/spring-cloud/service-client/src/main/java/com/spade/zhang/ServiceClientApplication.java
@@ -2,9 +2,11 @@ package com.spade.zhang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
@@ -12,6 +14,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServiceClientApplication {
 
   public static void main(String[] args) {
